f8f9dacfe542726244db466dc4a2547bf7418048
werronense <31091965+werronense@users.noreply.github.com>
2019-07-04 19:20:13 -0400

    Write basic functions and style .display

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const btns = document.querySelectorAll("button");
-  btns.forEach(btn => {
+  const display = document.querySelector(".display");
+  const numberBtns = document.querySelectorAll(".number");
+  numberBtns.forEach(btn => {
     btn.addEventListener("click", e => {
       console.log(e.target.id);
+      display.textContent += e.target.id;
     });
   });
+
+  function add(x, y) {
+    return x + y;
+  }
+
+  function subtract(x, y) {
+    return x - y;
+  }
+
+  function multiply(x, y) {
+    return x * y;
+  }
+
+  function divide(x, y) {
+    return x / y;
+  }
+
+  function operate(x, y, operator) {
+    let result = 0;
+
+    switch (expression) {
+      case "add":
+        result = add(x, y);
+        break;
+      case "subtract":
+        result = subtract(x, y);
+        break;
+      case "multiply":
+        result = multiply(x, y);
+        break;
+      case "divide":
+        result = divide(x, y);
+        break;
+      default:
+        result = "ERROR";
+    }
+    return result;
+  }
 });
