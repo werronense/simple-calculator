3fb06effc48d35f4922919f1ac11756377c58db8
werronense <31091965+werronense@users.noreply.github.com>
2019-07-05 07:19:45 -0400

    Add operator and equals functionality

--- main.js
+++ main.js
@@ -2,19 +2,63 @@ document.addEventListener("DOMContentLoaded", () => {
   const display = document.querySelector(".display");
   const numberBtns = document.querySelectorAll(".number");
   const operatorBtns = document.querySelectorAll(".operator");
+  const equalsBtn = document.querySelector("#equals");
 
+  let inputNumber = "";
+  let inputSeries = [];
+
+  // event listeners
   numberBtns.forEach(btn => {
     btn.addEventListener("click", e => {
-      display.textContent += e.target.id;
+      enterNumber(e);
     });
   });
 
   operatorBtns.forEach(btn => {
     btn.addEventListener("click", e => {
-      display.textContent += e.target.textContent;
+      enterOperator(e);
     });
   });
 
+  equalsBtn.addEventListener("click", () => {
+    handleInputNumber(inputNumber);
+    inputNumber = assessSeries(inputSeries);
+    display.textContent = inputNumber;
+    inputSeries = [];
+  });
+
+  // user interaction functions
+  function updateDisplay(input) {
+    display.textContent += input;
+  }
+
+  function handleInputNumber(number) {
+    inputSeries.push(Number(number));
+    inputNumber = "";
+  }
+
+  function enterNumber(e) {
+    updateDisplay(e.target.id);
+    inputNumber += e.target.id;
+  }
+
+  function enterOperator(e) {
+    updateDisplay(e.target.textContent);
+    handleInputNumber(inputNumber);
+    inputSeries.push(e.target.id);
+  }
+
+  function assessSeries(series) {
+    if (series.length === 3) {
+      return operate(series[0], series[2], series[1]);
+    } else {
+      const newSeries = series.slice(3);
+      newSeries.unshift(operate(series[0], series[2], series[1]));
+      assessSeries(newSeries);
+    }
+  }
+
+  // operator functions
   function add(x, y) {
     return x + y;
   }
@@ -34,7 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function operate(x, y, operator) {
     let result = 0;
 
-    switch (expression) {
+    switch (operator) {
       case "add":
         result = add(x, y);
         break;
